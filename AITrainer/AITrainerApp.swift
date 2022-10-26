0d74e32e9b9e904638b356617986435e1d48310b
An Byungwook <abw2619@icloud.com>
2022-10-26 18:38:34 +0900

    Initial Commit

---
+++ AITrainer/AITrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITrainerApp.swift
+//  AITrainer
+//
+//  Created by 안병욱의 mac on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct AITrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
