a86bbbda501257587d3b9dad1587725a632cc0c7
An Byungwook <abw2619@icloud.com>
2024-01-18 14:56:06 +0900

    1/18 ExerciseItem view

---
+++ AITrainer/UI/ExerciseItem.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseItem.swift
+//  AITrainer
+//
+//  Created by 안병욱의 mac on 2024/01/18.
+//
+
+import SwiftUI
+
+struct ExerciseItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseItem()
+}
